56bb3c121a1f8fd2f26d46b84962bcfc31204801
DESKTOP-VQ0K23P\SAFAYAT <safayat.borhan@selise.ch>
2020-12-26 18:54:44 +0600

    Add project files.

---
+++ Parallel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
+        }
+    }
+}
