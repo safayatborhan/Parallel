fe74098a41dbecd6f0f5c8989ee1333503386313
DESKTOP-VQ0K23P\SAFAYAT <safayat.borhan@selise.ch>
2020-12-26 21:42:43 +0600

    implement parallel for and foreach

--- Parallel/Program.cs
+++ Parallel/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Parallel
+namespace ParallelLoop
 {
     class Program
     {
+        public static IEnumerable<int> Range(int start, int end, int step)
+        {
+            for (int i = start; i < end; i+= step)
+            {
+                yield return i;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Main program done.");
-            Console.ReadKey();
+            var a = new Action(() => Console.WriteLine($"First {Task.CurrentId}"));
+            var b = new Action(() => Console.WriteLine($"Second {Task.CurrentId}"));
+            var c = new Action(() => Console.WriteLine($"Third {Task.CurrentId}"));
+
+            Parallel.Invoke(a,b,c);
+
+            Parallel.For(1, 11, i =>
+            {
+                Console.WriteLine($"{i * i}");
+            });
+
+            string[] words = {"oh", "what", "a", "night"};
+            Parallel.ForEach(words, word =>
+            {
+                Console.WriteLine($"{word} has length {word.Length}. Task: {Task.CurrentId}");
+            });
+
+            //If we want a custom one
+            Parallel.ForEach(Range(1, 20, 3), Console.WriteLine);
         }
     }
 }
